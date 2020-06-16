054ed6f89db1df3b910d660ebc0213c947366175
Craig McDaniel <cpmcdaniel@gmail.com>
2020-06-16 14:35:40 -0400

    Initial commit

---
+++ src/cpmcdaniel/vinyl/records.clj
@@ -0,0 +1,147 @@
+(ns cpmcdaniel.vinyl.records
+  (:require [clojure.string :as s]
+            [clj-time.core :as t]
+            [clj-time.format :as tf])
+  (:import (java.io Reader BufferedReader StringReader)))
+
+(defrecord Record [last-name first-name gender favorite-color date-of-birth])
+
+;; Delimiter data and logic
+(def pipe-delimiter #"\s*\|\s*")
+(def comma-delimiter #",\s*")
+(def space-delimiter #"\s+")
+
+(def delimiters [pipe-delimiter comma-delimiter space-delimiter])
+(def delimiter? (set delimiters))
+
+(defn check-delimiter!
+  "Throws an exception if the delimiter could not be identified."
+  [delimiter]
+  (if (delimiter? delimiter)
+    delimiter
+    (throw (IllegalArgumentException. "Delimiter cannot be determined from input."))))
+
+(defn get-delimiter
+  "Given a line of input, try to determine which delimiter to use.
+  Pipe is given highest priority, followed by comma, then space."
+  [line]
+  (check-delimiter!
+    (first
+     (filter
+       (fn [delimiter]
+         (= 5 (count (s/split line delimiter))))
+       delimiters))))
+
+;; Gender data and logic
+(def gender-set #{"M" "F"})
+
+(defn parse-gender
+  "Parses a gender string into one of 'M' or 'F'. Valid
+  inputs include (ingnoring case): 'M', 'F', 'male', 'female'"
+  [^String g]
+  (letfn [(short-gender [long-gender]
+            (get {"MALE" "M"
+                  "FEMALE" "F"}
+                 long-gender
+                 long-gender))]
+    (-> (.toUpperCase g)
+        short-gender
+        gender-set)))
+
+;; Date of birth logic
+(def multi-date-parser
+  (tf/formatter (t/default-time-zone)
+                :basic-date
+                :date
+                "MM/dd/yyyy"
+                "yyyy/MM/dd"))
+
+(defn parse-date
+  "Parses a date string. Accepts the following formats:
+  - 20200616
+  - 2020-06-16
+  - 06/16/2020
+  - 2020/06/16"
+  [date-string]
+  (tf/parse-local-date multi-date-parser date-string))
+
+(defn format-date
+  "Formats dates in MM/DD/YYYY format"
+  [dt]
+  (tf/unparse-local-date (tf/formatter "MM/dd/yyyy") dt))
+
+(defn strings->record
+  "Converts string arguments to a Record."
+  [[last-name first-name gender-string color-string dob-string]]
+  (->Record last-name
+            first-name
+            (parse-gender gender-string)
+            color-string
+            (parse-date dob-string)))
+
+(defn check-record!
+  "Throws an exception if the Record data is invalid."
+  [record]
+  (let [errors
+        (cond-> []
+                (= "" (:last-name record)) (conj "Last Name is a required field!")
+                (= "" (:first-name record)) (conj "First Name is a required field!")
+                (nil? (:gender record)) (conj "Gender must be M/F/male/female!")
+                (= "" (:favorite-color record)) (conj "Favorite color is a required field!")
+                (nil? (:date-of-birth record)) (conj "Invalid date of birth!"))]
+    (if (empty? errors)
+      record
+      (throw (IllegalArgumentException.
+               (str "Invalid record: "
+                    (s/join "\n" errors)))))))
+
+(defn make-line->record
+  "Creates a fn for converting lines to records, based on the given delimiter."
+  [delimiter]
+  (fn [line]
+    (let [args (map s/trim (s/split line delimiter))]
+      (if (= 5 (count args))
+        (check-record!
+          (strings->record args))
+        (throw (IllegalArgumentException.
+                 (format "Expected 5 fields per line, received %d from line: %s"
+                         (count args) line)))))))
+
+(defprotocol RecordReader
+  "Reads an input into a seq of Records"
+  (read-records [input]))
+
+(extend-protocol RecordReader
+  java.io.Reader
+  (read-records [input]
+    (let [lines (line-seq (BufferedReader. input))
+          delimiter (get-delimiter (first lines))
+          line->record (make-line->record delimiter)]
+      (map line->record lines)))
+
+  String
+  (read-records [input]
+    (read-records (StringReader. input))))
+
+(defn sort-by-gender
+  [records]
+  (sort-by :gender records))
+
+(defn sort-by-dob
+  [records]
+  (sort-by :date-of-birth records))
+
+(defn sort-by-last-name
+  [records]
+  (reverse (sort-by :last-name records)))
+
+(def join-fields #(s/join ", " %))
+
+(defn format-record
+  "Formats a Record as a string."
+  [rec]
+  (-> rec
+      (update :date-of-birth format-date)
+      (vals)
+      (join-fields)))
+
---
+++ test/cpmcdaniel/vinyl/records_test.clj
@@ -0,0 +1,63 @@
+(ns cpmcdaniel.vinyl.records-test
+  (:require [clojure.test :refer :all]
+            [cpmcdaniel.vinyl.records :as sut]
+            [clj-time.core :as t]))
+
+(deftest get-delimiter-test
+  (let [test-data
+        [["pipe" sut/pipe-delimiter "Wilson | Fred| male |blue | 01/22/2004"]
+         ["comma" sut/comma-delimiter "Wilson, Fred,male ,blue, 01/22/2004"]
+         ["space" sut/space-delimiter "Wilson Fred male blue 01/22/2004"]]]
+    (doseq [[test-name expected input-line] test-data]
+      (testing test-name
+        (is (= expected (sut/get-delimiter input-line)))))))
+
+(deftest check-delimiter!-test
+  (is (thrown? IllegalArgumentException
+               (sut/check-delimiter! nil)))
+  (doseq [delim (seq sut/delimiter?)]
+    (is (= delim (sut/check-delimiter! delim)))))
+
+(deftest read-string-test
+  (let [[fred jane] (sut/read-records "Wilson|Fred|male|blue|01/22/2004\nSmith|Jane|female|purple|04/13/2000")]
+    (is (= "Wilson" (:last-name fred)))
+    (is (= "Smith" (:last-name jane)))))
+
+(deftest parse-gender-test
+    (let [test-data [["M" ["M" "m" "MALE" "Male" "male"]]
+                     ["F" ["F" "f" "FEMALE" "Female" "female"]]]]
+      (doseq [[expected inputs] test-data]
+        (doseq [input inputs]
+          (let [actual (sut/parse-gender input)]
+            (is (= expected actual))))))
+    (is (nil? (sut/parse-gender "what is this?"))))
+
+(deftest parse-date-test
+  (let [good-data [[(t/local-date 2000 4 13) ["04/13/2000"
+                                             "20000413"
+                                             "2000-04-13"
+                                             "2000/04/13"]]]
+        bad-data ["13 JAN 2000" "13-JAN-2000" "blah"]]
+    (doseq [[expected inputs] good-data]
+      (doseq [input inputs]
+        (is (t/equal? expected (sut/parse-date input))
+            (format "%s did not parse as expected" input))))
+    (doseq [input bad-data]
+      (is (thrown? IllegalArgumentException
+                   (sut/parse-date input))))))
+
+(deftest sort-test
+  (let [fred (sut/->Record "Wilson" "Fred" "M" "blue" (sut/parse-date "20010304"))
+        jane (sut/->Record "Smith" "Jane" "F" "purple" (sut/parse-date "20010223"))
+        tom (sut/->Record "Apple" "Tom" "M" "green" (sut/parse-date "20010112"))
+        tests [[sut/sort-by-gender [fred jane] [jane fred]]
+               [sut/sort-by-dob [fred jane tom] [tom jane fred]]
+               [sut/sort-by-last-name [tom fred jane] [fred jane tom]]]]
+    (doseq [[sort-fn records expected] tests]
+      (is (= expected (sort-fn records))
+          (format "Sort failed for sort-fn: %s" sort-fn)))))
+
+(deftest format-record-test
+  (is (= "Wilson, Fred, M, blue, 03/24/2001"
+         (sut/format-record
+           (sut/->Record "Wilson" "Fred" "M" "blue" (sut/parse-date "20010324"))))))
