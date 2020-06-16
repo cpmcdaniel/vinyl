2d3e80ce392e96c5175b804a6b3e3ec23f653fd8
Craig McDaniel <cpmcdaniel@gmail.com>
2020-06-16 18:42:20 -0400

    Updated docstring for main

--- src/cpmcdaniel/vinyl.clj
+++ src/cpmcdaniel/vinyl.clj
@@ -7,7 +7,10 @@
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "When called with 0 args, will expose a REST API on port 8999.
+  Any arguments passed must be valid file paths and contain records
+  using any of the 3 delimiters (pipe, comma, space). If arguments are
+  passed, output is written to standard out."
   [& args]
   (if (pos? (count args))
     (->>
